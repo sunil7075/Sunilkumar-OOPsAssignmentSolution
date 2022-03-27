f1509b0e635de98cc676974fd083e1da7299402b
sunil7075 <b.sunil21@gmail.com>
2022-03-27 20:07:41 +0530

    OOPSLabassignment

---
+++ OOPsLabAssignment/src/com/greatlearning/departments/HrDepartment.java
@@ -0,0 +1,20 @@
+package com.greatlearning.departments;
+
+public class HrDepartment extends SuperDepartment {
+	public String departmentName() {
+		return "Hr Department";
+	}
+
+	public String getTodaysWork() {
+		return "Fill today’s worksheet and mark your attendance";
+	}
+
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+
+	public String doActivity() {
+		return "team Lunch";
+	}
+
+}
